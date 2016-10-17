7a7e8f57c7e48aa546460bd73b77504f28171383
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-17 19:19:10 -0400

    Added Headers to Table Class

--- my_table.class.php
+++ my_table.class.php
@@ -1,8 +1,18 @@
 <?php
 class MyTable{
-	function display($myarray){
+	function display($myarray, $head = False){
 		foreach( $myarray as $element ){
 			echo '<tr>';
+      if($head==True){
+        echo '<thead>';
+        foreach( $element as $key){
+          echo '<th>'.$key.'</th>';
+        }
+        $head = False;
+        echo '</thead>';
+        echo '</tr>';
+        continue;
+      }
 			foreach( $element as $key ){
 				echo '<td>'.$key.'</td>';
 			}
