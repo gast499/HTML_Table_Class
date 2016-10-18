ab2e7230866849341f3edde20b7e1f63351abcd2
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-18 14:05:29 -0400

    Added Explode Method

--- string.php
+++ string.php
@@ -24,6 +24,19 @@ foreach (count_chars($data2, 1) as $i => $val) {
    echo '<br>';
 }
 
+//using explode
+echo '<br><br>';
+echo 'Explode Method:';
+echo '<br>';
+$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+echo "$pizza";
+echo '<br>';
+$pieces = explode(" ", $pizza);//separate the pizza string by white space and store each substring as an element in an array 
+echo 'First 2 elements in the array after exploding the above string:';
+echo '<br>Pieces[0]: ';
+echo $pieces[0]; // piece1
+echo '<br>Pieces[1]: ';
+echo $pieces[1]; // piece2
 ?>
 	</body>
 </html>
