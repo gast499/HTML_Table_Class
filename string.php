89804da7c3ac22adb4950b37fc7a2e5cfdeebe90
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-18 14:56:18 -0400

    Added RTrim Method

--- string.php
+++ string.php
@@ -107,7 +107,20 @@ if (md5($str) === '1f3870be274f6c49b3e31a0c6728957f') {
     echo "Would you like a green or red apple?";
 }
 
-
+//using rtrim
+echo '<br><br>RTrim Method:<br>Before RTrim method:<br>';
+$text = "\t\tThese are a few words :) ...  ";
+$hello  = "Hello World";
+echo "$text" . '<br>' . "$hello" . '<br>';
+echo 'After RTrim method: <br>';
+$trimmed = rtrim($text);
+echo "$trimmed";
+echo '<br>';
+$trimmed = rtrim($text, " \t.");
+echo "$trimmed";
+echo '<br>';
+$trimmed = rtrim($hello, "Hdle");
+echo "$trimmed";
 ?>
 	</body>
 </html>
