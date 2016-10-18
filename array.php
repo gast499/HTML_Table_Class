20ccaf2e005f851ce2cf2002029b8a027dc3e87c
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-18 19:11:24 -0400

    Added Array Search Method

--- array.php
+++ array.php
@@ -17,6 +17,15 @@ $array = array("color" => array("blue", "red", "green"),
                "size"  => array("small", "medium", "large"));
 echo '<br>';
 print_r(array_keys($array));//print the keys in the multidimensional array
+
+//using array search
+echo '<br><br>Array Search Method:<br>The contents of the array are: <br>';
+$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+print_r($array);
+$key = array_search('green', $array); // $key = 2;
+echo '<br>The key for the "green" value is: ' . "$key";
+$key = array_search('red', $array);   // $key = 1;
+echo '<br>The key for the "red" value is: ' . "$key";
 ?>
 	</body>
 </html>
