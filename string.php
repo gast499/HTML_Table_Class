fc3cca0eb757146cc73dc2d389b864505245b427
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-18 15:31:49 -0400

    Added StrIPos Method

--- string.php
+++ string.php
@@ -143,6 +143,28 @@ echo '<br>Array after splitting string into elements 1 character long: <br>';
 print_r($arr1);
 echo '<br>Array after splitting string into elements up to 3 character long: <br>';
 print_r($arr2);
+
+//using stripos
+echo '<br><br>StrIPos Method:<br>';
+$findme    = 'a';
+$mystring1 = 'xyz';
+$mystring2 = 'ABC';
+
+$pos1 = stripos($mystring1, $findme);
+$pos2 = stripos($mystring2, $findme);
+
+// Nope, 'a' is certainly not in 'xyz'
+echo 'Searching for the string "a" in the string "xyz": ';
+if ($pos1 === false) {
+    echo "The string '$findme' was not found in the string '$mystring1'";
+}
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' is the 0th (first) character.
+echo '<br>Searching for the string "a" in the string "ABC": ';
+if ($pos2 !== false) {
+    echo "We found '$findme' in '$mystring2' at position $pos2";
+}
 ?>
 	</body>
 </html>
