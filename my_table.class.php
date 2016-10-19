659924894ff5579929ac190aecb7e04bef962d2e
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-19 16:04:24 -0400

    Updated the Table Class to accept Headers

--- my_table.class.php
+++ my_table.class.php
@@ -1,22 +1,36 @@
 <?php
 class MyTable{
 	function display($myarray, $head = False){
-		foreach( $myarray as $element ){
-			echo '<tr>';
-      if($head==True){
-        echo '<thead>';
-        foreach( $element as $key){
-          echo '<th>'.$key.'</th>';
-        }
-        $head = False;
-        echo '</thead>';
-        echo '</tr>';
-        continue;
+		if ($head == True){
+      $len = count($myarray[0]);
+      $akeys = array_keys($myarray);
+      echo '<tr><thead>';
+      foreach($akeys as $head){
+        echo '<th>'.$head.'</th>';
       }
-			foreach( $element as $key ){
-				echo '<td>'.$key.'</td>';
-			}
-			echo '</tr>';
-		}
+      echo '</thead></tr>';
+      $cur = "0";
+      foreach($myarray as $element){
+        $tmparray = array();
+        foreach($myarray as $innerarray){
+          array_push($tmparray, $innerarray[$cur]);
+        }
+        echo '<tr>';
+        foreach($tmparray as $key){
+          echo '<td>'.$key.'</td>';
+        }
+        echo '</tr>';
+        $cur+="1";
+      }
+    }
+    else{
+      foreach($myarray as $element){
+        echo '<tr>';
+        foreach($element as $key){
+          echo '<td>'.$key.'</td>';
+        }
+        echo '</tr>';
+      }
+    }
 	}
 }
