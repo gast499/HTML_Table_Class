a8090aaa5d0357b9ed406eb0faca06014b87a5cc
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-29 16:19:23 -0400

    Added strpos method

--- string.php
+++ string.php
@@ -210,7 +210,27 @@ echo "$str";
 echo '<br>The length of the string is: ';
 echo strlen($str); // 6
 
+//using strpos
+echo '<br><br>StrPos Method:<br>';
+$findme    = 'a';
+$mystring1 = 'ABC';
+$mystring2 = 'abc';
 
+$pos1 = strpos($mystring1, $findme);
+$pos2 = strpos($mystring2, $findme);
+
+// Nope, 'a' is certainly not in 'xyz'
+echo 'Searching for the string "a" in the string "ABC": ';
+if ($pos1 === false) {
+    echo "The string '$findme' was not found in the string '$mystring1'";
+}
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' is the 0th (first) character.
+echo '<br>Searching for the string "a" in the string "abc": ';
+if ($pos2 !== false) {
+    echo "We found '$findme' in '$mystring2' at position $pos2";
+}
 //using strtolower
 echo '<br><br>Str To Lower Method:<br>Before the Str To Lower method: ';
 $str = "Mary Had A Little Lamb and She LOVED It So";
